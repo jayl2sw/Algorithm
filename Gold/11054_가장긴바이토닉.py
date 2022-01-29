1fc34296c0669c26069caaa4dd6fb59c391d4733
jayl2sw <jayl2sf@gmail.com>
2022-01-29 21:48:05 +0900

    0129

---
+++ Gold/11054_가장긴바이토닉.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+n, m, v = map(int,input().split())
+
+dict = {}
+for i in range(m):
+    a, b = map(int,input().split())
+    dict[a] =
+
+print(dict)
+#
+# def bfs(start):
+#     q = deque()
+#     q.append(start)
+#
+#     while q:
+#         x = q.pop()
+#         for a in array:
+#
+#
+
+
+
